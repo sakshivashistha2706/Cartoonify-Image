2c3076a2dcdcc798f0cc4e960ee1b8264484a368
sakshivashistha2706 <56296624+sakshivashistha2706@users.noreply.github.com>
2022-07-02 17:29:28 +0530

    Update Cartoonify.py

--- Cartoonify.py
+++ Cartoonify.py
@@ -1,29 +1,34 @@
-import cv2 #for image processing
-import easygui #to open the filebox
+#importing necessary libraries
+import cv2
+import easygui
 import sys
 import matplotlib.pyplot as plt
 import os
 import tkinter as tk
 from tkinter import *
 
+#dimensions and display of user interface
 top=tk.Tk()
 top.geometry('400x400')
 top.title('Cartoonify Your Image !')
 top.configure(background='white')
 label=Label(top,background='#CDCDCD', font=('calibri',20,'bold'))
 
+#upload function to input image
 def upload():
     ImagePath=easygui.fileopenbox()
     cartoonify(ImagePath)
 
 
+#function to perform cartoonify action
 def cartoonify(ImagePath):
     # read the image
     originalmage = cv2.imread(ImagePath)
     originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
     #print(image)  # image is stored in form of numbers
 
-    # confirm that image is chosen
+    
+#error message if image is not uploaded
     if originalmage is None:
         print("Can not find any image. Choose appropriate file")
         sys.exit()
@@ -31,45 +36,49 @@ def cartoonify(ImagePath):
     ReSized1 = cv2.resize(originalmage, (960, 540))
 
 
-    #converting an image to grayscale
+#converting an image from color to grayscale
     grayScaleImage= cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
     ReSized2 = cv2.resize(grayScaleImage, (960, 540))
 
-    #applying median blur to smoothen an image
+#applying median blur for image smoothning
     smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
     ReSized3 = cv2.resize(smoothGrayScale, (960, 540))
 
-    #retrieving the edges 
-    #by using thresholding technique
+#retrieving the edges by using thresholding technique
     getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, 
         cv2.ADAPTIVE_THRESH_MEAN_C, 
         cv2.THRESH_BINARY, 9, 9)
 
     ReSized4 = cv2.resize(getEdge, (960, 540))
 
-    #applying bilateral filter to remove noise 
+
+#using bilateral filter to remove noise 
    
     colorImage = cv2.bilateralFilter(originalmage, 9, 300, 300)
     ReSized5 = cv2.resize(colorImage, (960, 540))
     
-    #masking edged image with our "BEAUTIFY" image
-    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
 
+#masking edged image with our "BEAUTIFY" image
+    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
     ReSized6 = cv2.resize(cartoonImage, (960, 540))
-    # Plotting the whole transition
+    
+# Plotting the transition
     images=[ReSized1, ReSized2, ReSized3, ReSized4, ReSized5, ReSized6]
 
     fig, axes = plt.subplots(3,2, figsize=(8,8), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
     for i, ax in enumerate(axes.flat):
         ax.imshow(images[i], cmap='gray')
 
+
+#defining buttons
     save1=Button(top,text="Save cartoon image",command=lambda: save(ReSized6, ImagePath),padx=30,pady=5)
     save1.configure(background='#364156', foreground='white',font=('calibri',10,'bold'))
     save1.pack(side=TOP,pady=50)
     
     plt.show()
     
-    
+
+#saving cartoonified image in same path as original image
 def save(ReSized6, ImagePath):
     #saving an image using imwrite()
     newName="CartoonifiedImage"
@@ -80,8 +89,12 @@ def save(ReSized6, ImagePath):
     I= "Image saved by name " + newName +" at "+ path
     tk.messagebox.showinfo(title=None, message=I)
 
+    
+#describing button features
 upload=Button(top,text="Cartoonify an Image",command=upload,padx=10,pady=5)
 upload.configure(background='#364156', foreground='white',font=('calibri',10,'bold'))
 upload.pack(side=TOP,pady=50)
 
+
+#loop to cartoonify multiple times
 top.mainloop()
